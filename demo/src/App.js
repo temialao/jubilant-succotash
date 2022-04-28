c85a0c01fdc665b2d1cf502a0222676000eeb8fa
good4something-6 <mail6mw@gmail.com>
2022-04-28 18:20:00 +0100

    Database pull and JSX BOTH WORKING

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import app from "./firebase";
-
-const db = getFirestore(app);
+import { db } from "./firebase";
 
 function App() {
   const [queryData, setQueryData] = useState([]);
