916fdf09d1a7fdba1dbb2c6be0c5f8408e61cf1f
good4something-6 <mail6mw@gmail.com>
2022-04-29 14:51:55 +0100

    messagingspike

--- demo/src/firebase.js
+++ demo/src/firebase.js
@@ -1,13 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getMessaging } from "firebase/messaging";
 
-import firebaseConfig from "./firebase.config";
+import { firebaseConfig, messPublicKey } from "./firebase.config";
 
 /// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+const messaging = getMessaging(app);
+messaging.getToken({ vapidKey: messPublicKey });
 
-export { auth, db };
+export { auth, db, messaging };
